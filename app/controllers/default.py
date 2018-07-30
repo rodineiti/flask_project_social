9371ff08ff5eda721cae68223d1971ea8567a463
unknown <rodinei.developer@hotmail.com>
2018-07-30 15:35:29 -0300

    Implementações da pagina de register para novos usuarios se registrarem no rede social

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -41,6 +41,24 @@ def login():
     
     return render_template('login.html', form=form)
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = UserForm()
+
+    if form.validate_on_submit():
+        #user = User.query.filter_by(username=form.username.data).first()
+        #if user:
+        #    flash("Usuário já existente")
+        #else:
+        #    flash("Invalid register.")
+        user = User(username=form.username.data, password=form.password.data, name=form.name.data, email=form.email.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Usuário criado com sucesso")
+        return redirect(url_for("index"))
+    
+    return render_template('register.html', form=form)
+
 @app.route('/logout')
 def logout():
     logout_user()
